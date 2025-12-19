0c4f7bac74f105c6635c42f1c8344de490401f4d
QAShyamThakkar <khimanishyam@gmail.com>
2025-12-19 09:39:54 +0530

    updated up to class05

---
+++ src/Class04_OperatorPart2/HW.java
@@ -0,0 +1,18 @@
+package Class04_OperatorPart2;
+
+public class HW {
+
+    static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+        int c = 2;
+
+        int result = a + b * c - a / c + b % c;
+        //10+5*2-10/2+5%2
+
+        System.out.println(result);
+
+
+    }
+}
---
+++ src/Class05_StringConcatenation/StringConcatenation.java
@@ -0,0 +1,33 @@
+package Class05_StringConcatenation;
+
+public class StringConcatenation {
+    static void main(String[] args) {
+        String str1 = "Hello";
+        String str2 = "World";
+        String space = " ";
+        int a = 10;
+        int b = 20;
+
+        System.out.println(str1); //Hello
+        System.out.println("str1"); //str1
+        System.out.println(str1 + space + str2);
+
+        System.out.println(str1 + a); //Hello10
+        System.out.println(str1 + str2 + a + b); //HelloWorld1020
+        System.out.println(a + b + str1);//30Hello
+
+        System.out.println(str1 + a + b); //hello1020
+        System.out.println(str1 + (a + b)); //hello30
+
+        System.out.println(str1 + a * b); //  hello200
+
+//        System.out.println(str1 + a - b); // error
+
+        System.out.println("str1" + a + b);
+
+    }
+
+}
+// int + int = add
+// int + String = Concat
+// String + int = Concat
