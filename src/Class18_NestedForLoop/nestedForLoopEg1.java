12f9c35d08caa34e98b8a42649c09b89d430931c
QAShyamThakkar <khimanishyam@gmail.com>
2026-01-21 09:12:05 +0530

    updated up to class18

---
+++ src/Class18_NestedForLoop/nestedForLoopEg1.java
@@ -0,0 +1,22 @@
+package Class18_NestedForLoop;
+
+public class nestedForLoopEg1 {
+    static void main(String[] args) {
+
+        //10 R --- 5 C
+        //Print from 1 to 5
+
+        for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("Hello ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+
+
+}
---
+++ src/Class18_NestedForLoop/nestedForLoopEg2.java
@@ -0,0 +1,21 @@
+package Class18_NestedForLoop;
+
+public class nestedForLoopEg2 {
+    static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+//            System.out.print(i +". ");
+
+            for (int j = 1; j <= 4; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+
+    }
+
+
+}
